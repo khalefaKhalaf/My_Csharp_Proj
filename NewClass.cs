7b6d43a0c5a8438f4767f1d0fe4084d0c5c01423
Khalefa_Khalaf <khlfkhlyfh10>
2025-09-14 16:01:21 +0300

    new class

---
+++ NewClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_project.My_Csharp_Proj
+{
+    internal class NewClass
+    {
+        public void fun()
+        {
+            Console.WriteLine("welcome in new class"); 
+        }
+    }
+}
